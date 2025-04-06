875ac77b6bc5be1852dce88e94b127a584de9810
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-04-06 13:25:15 +0700

    move parsing logic to function

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import pandas as pd
+from utils import *
 
-df = pd.read_parquet(
-    "./parsed/9e8d6ad0-1048-4909-8327-3eb857671aa2/bomb_events_2aa88ea8-3e89-4f63-a3c1-9cd90d041eae.parquet"
-)
-print(df.columns)
+parse_by_join_file()
