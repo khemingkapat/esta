7a83769f299ab10eb27b0d7409c6da33b70ced5b
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-04-19 15:08:47 +0700

    parse file

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 
 # parse_by_join_file create single table for each object as seen in ./parsed/
-# parse_by_join_file(
-#     dir_path="./decompressed", destination_path="./parsed", total_matches=50
-# )
+parse_by_join_file(
+    dir_path="./decompressed", destination_path="./parsed", total_matches=50
+)
 
 # parse_per_file create subfolder for each every match named after match_id
 # parse_per_file(
